5913bb7b869e7a3bda34ac17ee6453829c905e4c
ywyz <admin@ywyz.tech>
2023-10-09 21:16:28 +0800

    完成主功能页面的编写

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
 from UI.Login import Ui_Login
+from UI.Menu import Menu_Form
 from qt_material import apply_stylesheet
 
 if __name__ == "__main__":
@@ -10,8 +11,10 @@ if __name__ == "__main__":
     widget = QWidget()
     widget.setObjectName("MyWidget")
     widget.setStyleSheet("#MyWidget{border-image:url(Background.png);}")
-    ui = Ui_Login()
-    ui.setupUi(widget)
+    #ui = Ui_Login()
+    #ui.setupUi(widget)
+    Menu = Menu_Form()
+    Menu.setupUi(widget)
 
     widget.show()
     sys.exit(app.exec())
